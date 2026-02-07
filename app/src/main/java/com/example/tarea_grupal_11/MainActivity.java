fad0c4d26b4a2f63a61e9ac8ef7539a3cccd92bf
molina071 <7josueortega111@gmail.com>
2026-02-07 17:38:09 -0600

    actual

--- app/src/main/java/com/example/tarea_grupal_11/MainActivity.java
+++ app/src/main/java/com/example/tarea_grupal_11/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tarea_grupal_11;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int suma;
+
+    EditText num1 , num2;
+    Button btn_multiplicar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+         num1 = (EditText) findViewById(R.id.num1);
+         num2 = (EditText) findViewById(R.id.num2);
+
+        btn_multiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                multiplicar(num1, num2);
+            }
         });
+
+
+    }
+
+    private void multiplicar(int num1 , int num2 ) {
+
+        int resultado = num1 * num2;
+
+
     }
 }
