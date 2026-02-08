4abb6781c02e6af95ab72aae1d53dd758d221b8b
molina071 <7josueortega111@gmail.com>
2026-02-07 18:20:40 -0600

    multiplicacion

--- app/src/main/java/com/example/tarea_grupal_11/MainActivity.java
+++ app/src/main/java/com/example/tarea_grupal_11/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
 
     int suma;
 
-    EditText num1 , num2;
+    EditText num1 , num2, result;
     Button btn_multiplicar;
 
     @Override
@@ -25,24 +26,43 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-         num1 = (EditText) findViewById(R.id.num1);
-         num2 = (EditText) findViewById(R.id.num2);
+        btn_multiplicar = ( Button) findViewById(R.id.btn_multiplicar);
+
+
 
         btn_multiplicar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                multiplicar(num1, num2);
+                double valorNum1 = 0;
+                double valorNum2 = 0;
+
+                num1 = (EditText) findViewById(R.id.num1);
+                valorNum1 = Double.parseDouble(num1.getText().toString());
+                num2 = (EditText) findViewById(R.id.num2);
+                valorNum2 = Double.parseDouble(num1.getText().toString());
+                result = (EditText) findViewById(R.id.result);
+
+                if (valorNum1 < 0 || valorNum2 < 0) {
+                    Toast.makeText(getApplicationContext(), "los numero ingresados no deben ser menores a 0: ", Toast.LENGTH_LONG).show();
+                }else
+                multiplicar(valorNum1, valorNum2);
+                limpiar();
             }
         });
+    }
+    private void limpiar() {
+         num1.setText(null);
+         num2.setText(null);
+    }
+    private void multiplicar(double num1 , double num2 ) {
 
+        double multi = num1 * num2;
 
+        String resultado = String.valueOf(multi);
+
+        result.setText(resultado);
     }
 
-    private void multiplicar(int num1 , int num2 ) {
 
-        int resultado = num1 * num2;
-
-
-    }
 }
